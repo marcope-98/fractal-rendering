b16ea9fcedbd3190be0314f268740ff9a4ed71b1
marcope-98 <marco.eugenio.peressutti@gmail.com>
2025-11-10 18:55:20 +0100

    [frr] Adds constants to common header

---
+++ include/frr/common.hpp
@@ -0,0 +1,8 @@
+#ifndef FRR_COMMON_HPP_
+#define FRR_COMMON_HPP_
+namespace frr
+{
+  inline constexpr std::size_t width{1280};
+  inline constexpr std::size_t height{720};
+} // namespace frr
+#endif // FRR_COMMON_HPP_
