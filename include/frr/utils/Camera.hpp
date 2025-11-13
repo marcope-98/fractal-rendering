288f49ecad3cf481f74ae9c6b9ccccce189569b4
marcope-98 <marco.eugenio.peressutti@gmail.com>
2025-11-13 15:50:38 +0100

    [frr/Camera] Rewrites Camera to increase zoom depth

---
+++ include/frr/utils/Camera.hpp
@@ -0,0 +1,58 @@
+#ifndef FRR_UTILS_CAMERA_HPP_
+#define FRR_UTILS_CAMERA_HPP_
+#include "frr/utils/Vector.hpp"
+
+#include <cmath>
+
+namespace frr
+{
+  class Camera
+  {
+  public:
+    Vector_f64 target{0.0, 0.0};
+    Vector_f64 offset{0.0, 0.0};
+    double     zoom_factor{1.0};
+
+  public:
+    void pan(const Vector_f64 &delta) { this->target -= (delta / this->zoom_factor); }
+    void pan(Vector_f64 &&delta) { this->target -= (delta / this->zoom_factor); }
+
+    void zoom(const Vector_f64 &mouse_position, double wheel)
+    {
+      Vector_f64 mouseWorldPos = this->screen2world(mouse_position);
+      this->offset             = mouse_position;
+      this->target             = mouseWorldPos;
+      this->zoom_factor        = std::fmax(
+          std::exp(wheel * 0.2 + std::log(this->zoom_factor)),
+          1.0);
+    }
+
+    void reset()
+    {
+      this->target      = {0.0, 0.0};
+      this->offset      = {0.0, 0.0};
+      this->zoom_factor = 1.0;
+    }
+
+    void bound()
+    {
+      Vector_f64 boundary;
+      boundary = min(this->screen2world(this->world_lower_bound), this->world_lower_bound);
+      this->target -= boundary;
+
+      boundary = max(this->screen2world(this->world_upper_bound), this->world_upper_bound) - this->world_upper_bound;
+      this->target -= boundary;
+    }
+
+    Vector_f64 getTL() const { return this->screen2world(this->world_lower_bound); }
+    Vector_f64 getBR() const { return this->screen2world(this->world_upper_bound); }
+
+    Vector_f64 screen2world(const Vector_f64 &screen) const { return ((screen - this->offset) / this->zoom_factor) + this->target; }
+
+  private:
+    constexpr static Vector_f64 world_lower_bound{0.0, 0.0};
+    constexpr static Vector_f64 world_upper_bound{frr::width, frr::height};
+  };
+} // namespace frr
+
+#endif // FRR_UTILS_CAMERA_HPP_
--- main.cpp
+++ main.cpp
@@ -1,69 +1,14 @@
 #include <chrono>
 #include <cstdint>
+#include <iostream>
 #include <string>
 
 #include "raylib.h"
-#include "raymath.h"
 
 #include "frr/common.hpp"
 #include "frr/impl/naive.hpp"
 #include "frr/impl/simd.hpp"
-
-class CameraManager
-{
-public:
-  Camera2D camera{};
-
-  CameraManager()
-  {
-    this->camera.offset   = {0.f, 0.f};
-    this->camera.target   = {0.f, 0.f};
-    this->camera.rotation = 0.f;
-    this->camera.zoom     = 1.f;
-  }
-
-  void pan()
-  {
-    Vector2 delta       = GetMouseDelta();
-    delta               = Vector2Scale(delta, -1.f / this->camera.zoom);
-    this->camera.target = Vector2Add(this->camera.target, delta);
-  }
-
-  void zoom(float wheel)
-  {
-    Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), this->camera);
-    this->camera.offset   = GetMousePosition();
-    this->camera.target   = mouseWorldPos;
-    float scale           = 0.2f * wheel;
-    this->camera.zoom     = fmaxf(expf(logf(this->camera.zoom) + scale), 1.f);
-  }
-
-  void bound()
-  {
-    Vector2 boundary;
-    boundary            = GetScreenToWorld2D(this->world_lower_bound, camera);
-    boundary            = Vector2Min(boundary, this->world_lower_bound);
-    this->camera.target = Vector2Subtract(this->camera.target, boundary);
-
-    boundary            = GetScreenToWorld2D(this->world_upper_bound, camera);
-    boundary            = Vector2Subtract(Vector2Max(boundary, this->world_upper_bound), this->world_upper_bound);
-    this->camera.target = Vector2Subtract(this->camera.target, boundary);
-  }
-
-  Vector2 getTL() const { return GetScreenToWorld2D(this->world_lower_bound, camera); }
-  Vector2 getBR() const { return GetScreenToWorld2D(this->world_upper_bound, camera); }
-
-  void reset()
-  {
-    this->camera.zoom   = 1.f;
-    this->camera.offset = {0.f, 0.f};
-    this->camera.target = {0.f, 0.f};
-  }
-
-private:
-  const Vector2 world_lower_bound{0.f, 0.f};
-  const Vector2 world_upper_bound{frr::width, frr::height};
-};
+#include "frr/utils/Camera.hpp"
 
 int main(void)
 {
@@ -78,16 +23,25 @@ int main(void)
   };
 
   InitWindow(frr::width, frr::height, "Fractal Rendering");
-  CameraManager cm;
-  Texture2D     texture          = LoadTextureFromImage(image);
-  int           last_key_pressed = KEY_NULL;
+  frr::Camera cm;
+  Texture2D   texture          = LoadTextureFromImage(image);
+  int         last_key_pressed = KEY_NULL;
   while (!WindowShouldClose())
   {
     // User Input
     if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) cm.reset();
-    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) cm.pan();
-    if (float wheel = GetMouseWheelMove(); wheel != 0) cm.zoom(wheel);
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+    {
+      Vector2 delta = GetMouseDelta();
+      cm.pan({delta.x, delta.y});
+    }
+    if (float wheel = GetMouseWheelMove(); wheel != 0)
+    {
+      Vector2 mousePosition = GetMousePosition();
+      cm.zoom({mousePosition.x, mousePosition.y}, static_cast<double>(wheel));
+    }
     cm.bound();
+
     if (IsKeyPressed(KEY_KP_ADD)) max_iterations += 32;
     if (IsKeyPressed(KEY_KP_SUBTRACT)) max_iterations = max_iterations == 32 ? 32 : max_iterations - 32;
     if (int key = GetKeyPressed(); KEY_ONE <= key && key <= KEY_TWO) last_key_pressed = key;
