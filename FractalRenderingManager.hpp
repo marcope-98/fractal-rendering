4ccf67d9d94c6a001dc5db460a1e7963bf3fd8c9
marcope-98 <marco.eugenio.peressutti@gmail.com>
2025-11-13 21:51:54 +0100

    [main] Cleans up main implementation

---
+++ FractalRenderingManager.hpp
@@ -0,0 +1,113 @@
+#ifndef FRACTALRENDERINGMANAGER_HPP_
+#define FRACTALRENDERINGMANAGER_HPP_
+
+#include <cstdint>
+#include <string>
+
+#include "raylib.h"
+
+#include "frr/common.hpp"
+#include "frr/impl/naive.hpp"
+#include "frr/impl/simd.hpp"
+#include "frr/impl/threadpool.hpp"
+#include "frr/impl/threads.hpp"
+#include "frr/utils/Camera.hpp"
+
+struct FractalRenderingManager
+{
+  // fractal rendering
+  uint8_t *       data{nullptr};
+  std::size_t     max_iterations{64};
+  frr::ThreadPool tp;
+
+  // raylib
+  Texture2D   texture;
+  std::string methods[4] = {"Naive", "AVX2", "Threads", "ThreadPool"};
+
+  // user input
+  frr::Camera cm;
+  int         last_key_pressed{0};
+
+  FractalRenderingManager()
+  {
+    this->data = new std::uint8_t[frr::width * frr::height]();
+    this->tp.init(this->data);
+
+    InitWindow(frr::width, frr::height, "Fractal Rendering");
+    Image image = {
+        this->data,                         /* data */
+        frr::width,                         /* width */
+        frr::height,                        /* height */
+        PIXELFORMAT_UNCOMPRESSED_GRAYSCALE, /* format */
+        1                                   /* mipmaps */
+    };
+    this->texture = LoadTextureFromImage(image);
+  }
+
+  ~FractalRenderingManager()
+  {
+    UnloadTexture(this->texture);
+    CloseWindow();
+    this->tp.shutdown();
+    delete[] this->data;
+  }
+
+  void user_input()
+  {
+    // Camera controls
+    if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+      this->cm.reset();
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+    {
+      Vector2 delta = GetMouseDelta();
+      cm.pan({delta.x, delta.y});
+    }
+    if (float wheel = GetMouseWheelMove(); wheel != 0)
+    {
+      Vector2 mousePosition = GetMousePosition();
+      cm.zoom({mousePosition.x, mousePosition.y}, static_cast<double>(wheel));
+    }
+    cm.bound();
+
+    // Keypad controls
+    if (IsKeyPressed(KEY_KP_ADD)) this->max_iterations += 32;
+    if (IsKeyPressed(KEY_KP_SUBTRACT)) this->max_iterations = this->max_iterations == 32 ? 32 : this->max_iterations - 32;
+
+    // Method selection
+    if (int key = GetKeyPressed(); KEY_ONE <= key && key <= KEY_FOUR) this->last_key_pressed = key - KEY_ONE;
+  }
+
+  void run()
+  {
+    switch (this->last_key_pressed)
+    {
+      case 0:
+        frr::naive(this->data, this->cm.getTL(), this->cm.getBR(), this->max_iterations);
+        break;
+      case 1:
+        frr::simd(this->data, this->cm.getTL(), this->cm.getBR(), this->max_iterations);
+        break;
+      case 2:
+        frr::threads(this->data, this->cm.getTL(), this->cm.getBR(), this->max_iterations);
+        break;
+      case 3:
+        tp.run(this->cm.getTL(), this->cm.getBR(), this->max_iterations);
+        break;
+    }
+  }
+
+  void render(const std::string &duration)
+  {
+    UpdateTexture(this->texture, this->data);
+    // clang-format off
+    BeginDrawing();
+      DrawTexture(this->texture, 0, 0, WHITE);
+      DrawText((duration + " [ms] | Max Iterations: " + std::to_string(this->max_iterations) + 
+                                " | " + this->methods[this->last_key_pressed]).c_str(),
+                0, 0, 20, RAYWHITE);
+    EndDrawing();
+    // clang-format on
+  }
+};
+
+#endif // FRACTALRENDERINGMANAGER_HPP_
--- main.cpp
+++ main.cpp
@@ -1,88 +1,21 @@
 #include <chrono>
-#include <cstdint>
-#include <iostream>
-#include <string>
 
+#include "FractalRenderingManager.hpp"
 #include "raylib.h"
-
-#include "frr/common.hpp"
-#include "frr/impl/naive.hpp"
-#include "frr/impl/simd.hpp"
-#include "frr/impl/threadpool.hpp"
-#include "frr/impl/threads.hpp"
-#include "frr/utils/Camera.hpp"
 
 int main(void)
 {
-  std::size_t max_iterations{64};
-  std::string methods[4] = {"Naive", "AVX2", "Threads", "ThreadPool"};
-  uint8_t *   data       = new uint8_t[frr::width * frr::height]();
-  Image       image      = {
-      data,                               /* data */
-      frr::width,                         /* width */
-      frr::height,                        /* height */
-      PIXELFORMAT_UNCOMPRESSED_GRAYSCALE, /* format */
-      1                                   /* mipmaps */
-  };
-
-  InitWindow(frr::width, frr::height, "Fractal Rendering");
-
-  frr::Camera     cm;
-  frr::ThreadPool tp(data);
-  Texture2D       texture          = LoadTextureFromImage(image);
-  int             last_key_pressed = 0;
+  FractalRenderingManager frm;
   while (!WindowShouldClose())
   {
-    // User Input
-    if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) cm.reset();
-    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
-    {
-      Vector2 delta = GetMouseDelta();
-      cm.pan({delta.x, delta.y});
-    }
-    if (float wheel = GetMouseWheelMove(); wheel != 0)
-    {
-      Vector2 mousePosition = GetMousePosition();
-      cm.zoom({mousePosition.x, mousePosition.y}, static_cast<double>(wheel));
-    }
-    cm.bound();
+    frm.user_input();
 
-    if (IsKeyPressed(KEY_KP_ADD)) max_iterations += 32;
-    if (IsKeyPressed(KEY_KP_SUBTRACT)) max_iterations = max_iterations == 32 ? 32 : max_iterations - 32;
-    if (int key = GetKeyPressed(); KEY_ONE <= key && key <= KEY_FOUR) last_key_pressed = key - KEY_ONE;
-
-    // Fractal computation
     auto begin = std::chrono::steady_clock::now();
-    switch (last_key_pressed)
-    {
-      case 0:
-        frr::naive(data, cm.getTL(), cm.getBR(), max_iterations);
-        break;
-      case 1:
-        frr::simd(data, cm.getTL(), cm.getBR(), max_iterations);
-        break;
-      case 2:
-        frr::threads(data, cm.getTL(), cm.getBR(), max_iterations);
-        break;
-      case 3:
-        tp.run(cm.getTL(), cm.getBR(), max_iterations);
-        break;
-    }
+    frm.run();
     auto end = std::chrono::steady_clock::now();
-    // Update Texture with new screen buffer
-    UpdateTexture(texture, data);
-    // Draw Texture
-    BeginDrawing();
-    DrawTexture(texture, 0, 0, WHITE);
-    DrawText((std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()) + " [ms] | Max Iterations: " + std::to_string(max_iterations) + " | " + methods[last_key_pressed]).c_str(),
-             0, 0, 20, RAYWHITE);
-    EndDrawing();
+
+    frm.render(std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()));
   }
 
-  tp.shutdown();
-  // Clean up
-  UnloadTexture(texture);
-  CloseWindow();
-  delete[] data;
   return 0;
 }
