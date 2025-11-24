c379eebff45c59ca813bac0aa9ad276495405bdf
marcope-98 <marco.eugenio.peressutti@gmail.com>
2025-11-24 12:15:46 +0100

    [cmake] Makes FetchContent faster by Shallow cloning the repo

--- cmake/ImportRaylibTarget.cmake
+++ cmake/ImportRaylibTarget.cmake
@@ -1,8 +1,11 @@
 include(FetchContent)
+set(FETCHCONTENT_QUIET FALSE CACHE INTERNAL "")
 FetchContent_Declare(
   raylib
   GIT_REPOSITORY https://github.com/raysan5/raylib.git
   GIT_TAG        5.5
+  GIT_PROGRESS   ON
+  GIT_SHALLOW    1
   )
 set(BUILD_EXAMPLES OFF CACHE INTERNAL "")
 FetchContent_MakeAvailable(raylib)
