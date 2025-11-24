4916a88d3b9f7bba21c91452945fa7ebb4284f21
marcope-98 <marco.eugenio.peressutti@gmail.com>
2025-11-24 12:33:06 +0100

    [main] Removes redundant 'Error' in catch block

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
   }
   catch (const std::exception &e)
   {
-    std::cerr << "Error: " << e.what() << "\n";
+    std::cerr << e.what() << "\n";
     return EXIT_FAILURE;
   }
 
