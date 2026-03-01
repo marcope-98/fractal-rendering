e3d5080055c09da76a529ab252386f6e42cf5a71
PERESSUTTI Marco (Iveco Group) <marco.peressutti@external.ivecogroup.com>
2026-03-01 10:46:28 +0100

    [cmake] Removes unused cmake macros and functions

--- cmake/IgnoreExternalHeadersWarning.cmake
+++
@@ -1,4 +0,0 @@
-function(ignore_external_headers_warning target external_library_target)
-  get_target_property(lib_include_dirs ${external_library_target} INTERFACE_INCLUDE_DIRECTORIES)
-  target_include_directories(${target} SYSTEM PRIVATE ${lib_include_dirs})
-endfunction(ignore_external_headers_warning)
